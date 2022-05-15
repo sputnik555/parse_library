0fa2d1310285c2c63d6bae34e59a02a83e622982
sputnik555 <79382246+sputnik555@users.noreply.github.com>
2022-05-15 04:16:24 +0300

    download books with titles.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import requests
+import os
 from pathlib import Path
+from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filename
 
 
 def check_for_redirect(response):
@@ -7,16 +10,36 @@ def check_for_redirect(response):
         raise requests.HTTPError
 
 
+def download_txt(url, filename, folder='books/'):
+    """Функция для скачивания текстовых файлов.
+    Args:
+        url (str): Cсылка на текст, который хочется скачать.
+        filename (str): Имя файла, с которым сохранять.
+        folder (str): Папка, куда сохранять.
+    Returns:
+        str: Путь до файла, куда сохранён текст.
+    """
+    Path(folder).mkdir(exist_ok=True)
+    response = requests.get(url)
+    response.raise_for_status()
+    path = os.path.join(folder, sanitize_filename(f'{filename}.txt'))
+    with open(path, 'wb') as file:
+        file.write(response.content)
+    return path
+
+
 if __name__ == '__main__':
-    Path('book').mkdir(exist_ok=True)
     for book_id in range(1, 11):
-        url = "https://tululu.org/txt.php?id={}".format(book_id)
+        url = "https://tululu.org/b{}/".format(book_id)
         response = requests.get(url)
         try:
             check_for_redirect(response)
         except requests.HTTPError:
             continue
         response.raise_for_status()
-        filename = 'book/{}.txt'.format(book_id)
-        with open(filename, 'wb') as file:
-            file.write(response.content)
+        soup = BeautifulSoup(response.text, 'lxml')
+        tululu_title = soup.find('td', class_='ow_px_td').find('h1').text.split('::')
+        book_title = tululu_title[0].strip()
+        book_author = tululu_title[1].strip()
+        url = "https://tululu.org/txt.php?id={}".format(book_id)
+        download_txt(url, f'{book_id}.{book_title}')
